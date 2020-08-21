11a1ab050abf9449d44606342e4bdeceaa800687
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-08-21 22:22:39 +0100

    Class connection!
    Connection to the DataBase!

--- src/Data/Conexao.java
+++ src/Data/Conexao.java
@@ -5,6 +5,18 @@ package Data;
  *
  * @author Fenias
  */
+import java.sql.*;
+
 public class Conexao {
+    private Connection con;
+
+    public Conexao() throws Exception{
+        Class.forName("net.sourceforge.jtds.jdbc.Driver");
+        String url = "jdbc:jtds:sqlserver://FENIAS-MANHENGE\\XIMIX:1433/EMPLOYEE";
+        con = DriverManager.getConnection(url, "employee", "13579");
+    }
     
+    public Connection getConnection(){
+        return  con;
+    }
 }
